10b9ad5ffcf539e6f80c48d9fdbd8b063bea58fa
noymaxx <hugo.povoleri@sou.inteli.edu.br>
2023-02-02 13:16:58 -0300

    Atividade Ponderada 1 de Programação finalizada

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 1 # variável que define o status, se o jogo está rodando ou está parado
+var vscore = 0 # variável que controla a pontuação
+var x = 1.5 # variável que controla a posição e a velocidade do background no eixo horizontal
+var y = 2.0 # variável que controla a posição e a velocidade do dragão no eixo vertical 
 
 # executa essa função ao carregar o jogo
 func _ready():
